5d0c3811a03af0ade053842998d96ea3145a4fb6
maeganskinnerblog <maeganskinnerblog@gmail.com>
2019-09-06 10:59:56 -0500

    initialCommit

---
+++ skillscheck-2-app/skillscheck-2-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from "./components/Header";
+import Form from "./components/Form";
+import Product from "./components/Product";
+import Dashboard from "./components/Dashboard";
+//import './App.css';
+
+function App() {
+  render() {
+    <Header />
+    <Dashboard />
+    <Form />
+  }
+}
+
+export default App;
